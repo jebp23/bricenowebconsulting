4a0303959a8a3588ca1fc078ef7893a79bd6e65f
jebp23 <jebp2389@gmail.com>
2020-12-13 16:33:27 -0300

    Update message.service.ts

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -8,6 +8,6 @@ export class MessageService {
   constructor(private _http: HttpClient) { }
   
   sendMessage(body) {
-    return this._http.post(process.env.PORT || 'http://localhost:8080/', body);
+    return this._http.post('http://localhost:8080/', body);
   }
 }
