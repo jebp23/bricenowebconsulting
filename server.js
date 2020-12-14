5de5230479e60d44d77bc15866aefeeee759cc67
jebp23 <jebp2389@gmail.com>
2020-12-14 00:10:50 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const configEmail = require('./emailConfig/emailConfig');
 const path = require('path');
 
@@ -6,6 +7,7 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
 app.post('/form', (req, res) => {  
     configEmail(req.body);
