ebf73df45aa4cd5dfd56d627d792a1fd3409cb55
jebp23 <jebp2389@gmail.com>
2020-12-14 22:19:00 -0300

    lala

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+
 @Injectable()
 
 export class MessageService {
   
   constructor(private _http: HttpClient) { }
   
-  sendMessage(body) {
-    return this._http.post('contact/contactForm', body);
+  sendMessage(form) {
+    return this._http.post('contact/contactForm', form);
   }
 }
