2d91054c08c20b85330da23a53c3c7400ef35226
jebp23 <jebp2389@gmail.com>
2020-12-15 22:10:17 -0300

    service correction

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -8,7 +8,7 @@ export class MessageService {
   constructor(private _http: HttpClient) { }
   
   sendMessage(body) {
-    return this._http.post('contact/contactForm', body);
+    return this._http.post('process.env.PORT/contactForm || 8080/contactForm', body);
   }
 }
 
