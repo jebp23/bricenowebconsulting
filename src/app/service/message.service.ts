feb6deb48875675da71b188dea2415422e54a99b
jebp23 <jebp2389@gmail.com>
2020-12-15 23:18:52 -0300

    services and server fix

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -15,14 +15,3 @@ export class MessageService {
     return this._http.post('https://bricenowebconsulting.herokuapp.com/contactForm', form, {headers});
   }
 }
-
-/*
-Schema validation failed with the following errors:
-
-  Data path ".builders['app-shell']" should have required property 'class'.
-
-Error: Schema validation failed with the following errors:
-
-  Data path ".builders['app-shell']" should have required property 'class'.
-
-    at MergeMapSubscriber.project (/tmp/build_d2427bb9_/node_modules/@angular/cli/node_modules/@angular-devkit/core/src/workspace/workspace.js:215:42)*/
