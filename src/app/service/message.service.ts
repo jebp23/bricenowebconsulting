3c27081cd9496c80e82da009a197864a5fc9240e
jebp23 <jebp2389@gmail.com>
2020-12-16 00:12:32 -0300

    Update message.service.ts

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -1,13 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Email } from '../models/email';
 
 @Injectable()
 
 export class MessageService {  
   constructor(private _http: HttpClient) { }
   
-  sendMessage(body:Email) {
+  sendMessage(body) {
 
     return this._http.post('https://bricenowebconsulting.herokuapp.com/contactForm', body);
   }
