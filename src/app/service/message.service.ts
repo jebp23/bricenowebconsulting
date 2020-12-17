27a086fb085ffc204774097cd0c0445d3a9c9ac8
jebp23 <jebp2389@gmail.com>
2020-12-17 18:38:26 -0300

    Update message.service.ts

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -4,12 +4,10 @@ import { HttpClient } from '@angular/common/http';
 @Injectable()
 
 export class MessageService {
-  private contactFormUrl = '/form';
   
   constructor(private _http: HttpClient) { }
   
-  sendMessage(newEmail) {
-
-    return this._http.post(this.contactFormUrl, newEmail);
+  sendMessage(body) {
+    return this._http.post('http://localhost:8080/form', body);
   }
 }
