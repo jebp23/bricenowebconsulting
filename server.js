491777fe62fe90ca4594f60f61f1d0790bf93e2b
jebp23 <jebp2389@gmail.com>
2020-12-21 01:54:18 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express();
 //Body Parser/Cors-Headers Middleware
 //app.use(express.urlencoded({extended: true}))
 app.use(express.json());
-app.use(express.urlencoded())
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
