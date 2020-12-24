c99b8a4435a7f978faaa826e8f4f4a05ad2a5f62
jebp23 <jebp2389@gmail.com>
2020-12-24 12:00:28 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,16 @@
 const express = require('express');
 const app = express();
 
-
 //Body Parser/Cors-Headers Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});*/
+});
 
 //Angular project file routing
 var distDir = __dirname + "/dist";
