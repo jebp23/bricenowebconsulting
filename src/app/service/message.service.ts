884a692d1917dddde9d556c05577189c8b188f2c
jebp23 <jebp2389@gmail.com>
2020-12-29 17:54:07 -0300

    Update message.service.ts

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
-//import { Email } from '../models/email';
+import { Email } from '../models/email';
 
 @Injectable()
 
