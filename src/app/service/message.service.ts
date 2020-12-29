33f200d75035b55b51de304d4ed6cd625af8265a
jebp23 <jebp2389@gmail.com>
2020-12-29 00:39:41 -0300

    update

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-/*import { Observable } from 'rxjs';
-import { Email } from '../models/email';*/
+import { Observable } from 'rxjs';
+//import { Email } from '../models/email';
 
 @Injectable()
 
@@ -12,8 +12,8 @@ export class MessageService {
 
   }
   
-  sendMessage(body) {
+  sendMessage(body): Observable<any>{
     let headers = new HttpHeaders().set('Content-Type','application/json');
-    return this._http.post(this.formUrl, /*JSON.stringify(body)*/body, {headers});
+    return this._http.post(this.formUrl, JSON.stringify(body), {headers});
   }
 }
