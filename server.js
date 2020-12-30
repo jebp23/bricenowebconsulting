7fd8db1f19a273971a4b4da0b5572663224975dc
jebp23 <jebp2389@gmail.com>
2020-12-30 19:29:44 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require('express');
-const cors = require('cors');
 const nodemailer = require("nodemailer");
 const app = express();
 
 //Body Parser/Cors-Headers Middleware
 app.use(express.json());
-app.use(cors())
-/*app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
     res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
     next();
-});*/
+});
 
 //Angular project file routing
 var distDir = __dirname + "/dist";
