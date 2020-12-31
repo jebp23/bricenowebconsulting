b67dcb7a96aa94805acbf106b4c3bafc8b9d5af1
jebp23 <jebp2389@gmail.com>
2020-12-31 08:29:31 -0300

    final fixes

--- Backend/apiRouter.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var formController = require('./apiController');
-
-var router = express.Router();
-router.post('/form', formController.sendEmail);
-
-
-module.exports = router;
--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -11,28 +11,3 @@ export class MessageService {
     return this.http.post('https://bricenowebconsulting.herokuapp.com/formApi', body);  
   }
 }
-
-/* 
-    const CLIENT_ID='317858752894-jbje31oh7pc30l3al6kl129k2mnq279k.apps.googleusercontent.com';
-    const CLIENT_SECRET='LF0qv1-FwH1PXq6o9eoprp1A';
-    const REDIRECT_URI='https://developers.google.com/oauthplayground';
-    const REFRESH_TOKEN='1//04yHt6RuEegGACgYIARAAGAQSNwF-L9Ir5qAF72bljOpBFlEvnDOHI_ZeUItjIKBfjfplG9pHJSxcUZyhbAdDjFq_RxrLIu1RR2E';
-    
-    const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
-    oAuth2Client.setCredentials({refresh_token: REFRESH_TOKEN});
-
-    function sendMail(){
-        try{
-            const accessToken = await oAuth2Client.getAccessToken();
-            const transporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    type:'OAuth2',
-                    user: 'jebp2389@gmail.com',
-                    clientId: CLIENT_ID,
-                    clientSecret: CLIENT_SECRET,
-                    refreshToken: REFRESH_TOKEN,
-                    accessToken: accessToken            
-                }
-            });
-*/
